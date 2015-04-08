6bbedf720c20ca74414e3d817729ccacc51fddb9
getzze <getzze@gmail.com>
2015-04-08 18:55:43 +0100

    Patch tyrannical

---
+++ lib/patch_move_client.lua
@@ -0,0 +1,54 @@
+-- Patch for the awful.client.movetoscreen and awful.client.movetotag functions to recompute the maximized and fullscreen values
+-- Load this patch after loading awful in your configuration file
+local awful     = require("awful")
+
+local capi      = {
+   client = client
+}
+
+local orig_movetotag    = awful.client.movetotag
+local orig_movetoscreen = awful.client.movetoscreen
+
+local function reload_max (c)
+    if c.maximized then
+        c.maximized = false
+        c.maximized = true
+    else
+        if c.maximized_horizontal then
+            c.maximized_horizontal = false
+            c.maximized_horizontal = true
+        end
+        if c.maximized_vertical then
+            c.maximized_vertical = false
+            c.maximized_vertical = true
+        end
+    end
+    if c.fullscreen then
+        c.fullscreen = false
+        c.fullscreen = true
+    end
+end
+
+-- Replacement for awful.client.movetotag
+local function movetotag(target, c)
+    local sel = c or capi.client.focus
+    local s = awful.tag.getscreen(target)
+
+    if s == sel.screen then
+        orig_movetotag(target, c)
+    else
+        orig_movetotag(target, sel)
+        reload_max(sel)
+    end
+end
+
+-- Replacement for awful.client.movetoscreen
+local function movetoscreen(c, s)
+    local sel = c or capi.client.focus
+    orig_movetoscreen(sel, s)
+    reload_max(sel)
+end
+
+-- Monkey patch
+awful.client.movetotag    = movetotag
+awful.client.movetoscreen = movetoscreen
