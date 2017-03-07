5e8cc7b4585b70861cb4287bc14ac3739b6b6346
getzze <getzze@gmail.com>
2017-03-07 17:40:19 +0000

    Add themes

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -1,30 +1,15 @@
 ---------------------------
--- fork of
--- japanese2 awesome theme 
+-- Default awesome theme --
 ---------------------------
+local util = require('awful.util')
 
-local home_dir = os.getenv("HOME")
-local theme_dir = home_dir .. "/.config/awesome/themes/default"
-
-local function rgb(red, green, blue)
-  if type(red) == "number" or type(green) == "number" or type(blue) == "number" then
-    return "#"..string.format("%02x",red)..string.format("%02x",green)..string.format("%02x",blue)
-  else    
-    return nil
-  end
+function script_path()
+    local debug = require("debug")
+    --local str = debug.getinfo(2, "S").source:sub(2)
+    local str = debug.getinfo(1).source:match("@(.*)$")
+    return str:match("(.*/)")
 end
 
-local function rgba(red, green, blue, alpha) 
-  if type(red) == "number" or type(green) == "number" or type(blue) == "number" or type(alpha) == "number" then
-    return "#"..string.format("%02x",red)..string.format("%02x",green)..string.format("%02x",blue)..string.format("%02x",alpha * 255)
-  else
-    return nil
-  end
-end
-
---colors
-local bordeaux          = rgb(47,28,28)
-local light_bordeaux    = rgba(191,64,64,0.6)
 local dark_grey         = "#121212"
 local grey              = "#444444ff"
 local light_grey        = "#555555"
@@ -49,32 +34,13 @@ local widget_background = "#303030"
 --local white = "#B0B0B0"
 local bright_white      = "#F7F7F7"
 local transparent       = "#00000000"
---background=#121212
---foreground=#aeafad
 
+----------
+local theme = {}
 
-theme = {}
-theme.grey = grey
-theme.light_grey = light_grey
-theme.white = white
-theme.black = black
-theme.light_black = light_black
-theme.red = red
-theme.bright_red = bright_red
-theme.yellow = yellow
-theme.bright_yellow = bright_yellow
-theme.bright_black = bright_black
-theme.green = green
-theme.bright_green =bright_green
-theme.blue = blue
-theme.bright_blue = bright_blue
-theme.magenta = magenta
-theme.bright_magenta = bright_magenta
-theme.cyan = cyan
-theme.bright_cyan = bright_cyan
-theme.widget_background = widget_background
-theme.transparent = transparent
-theme.font          = "Droid Sans 8"
+--theme.dir           = os.getenv("HOME") .. "/.config/awesome/themes/default"
+theme.dir           = script_path()
+theme.font          = "Droid sans 8"
 
 theme.bg_normal     = light_black 
 theme.bg_focus      = red
@@ -85,22 +51,25 @@ theme.bg_systray    = theme.bg_normal
 theme.fg_normal     = light_white
 theme.fg_focus      = white
 theme.fg_urgent     = black
---theme.fg_minimize   = black
 theme.fg_minimize   = blue
 
+theme.useless_gap   = 0
 theme.border_width  = 0 
 theme.border_normal = light_black
 theme.border_focus  = light_white
 theme.border_marked = theme.bg_normal
 
+
 -- There are other variable sets
 -- overriding the default one when
 -- defined, the sets are:
--- [taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty]
+-- tasklist_[bg|fg]_[focus|urgent]
 -- titlebar_[bg|fg]_[normal|focus]
 -- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
 -- mouse_finder_[color|timeout|animate_timeout|radius|factor]
 -- Example:
+--theme.taglist_bg_focus = "#ff0000"
 theme.tasklist_bg_focus    = light_grey
 theme.tasklist_fg_focus    = yellow 
 theme.tasklist_bg_minimize = bright_blue
@@ -108,32 +77,30 @@ theme.tasklist_fg_minimize = black
 theme.titlebar_bg_normal   = light_black
 theme.titlebar_bg_focus    = light_black 
 
---theme.taglist_bg_normal= "#333333"
---theme.titlebar_bg_normal =
---theme.titlebar_bg_focus =
 -- Display the taglist squares
-theme.taglist_squares_sel   = theme_dir.."/taglist/squaref.png"
-theme.taglist_squares_unsel = theme_dir.."/taglist/square.png"
+--theme.taglist_squares_sel   = theme.dir.."/taglist/bar_sel.png"
+--theme.taglist_squares_unsel = theme.dir.."/taglist/bar_unsel.png"
+theme.taglist_squares_sel   = theme.dir.."/taglist/squarefw.png"
+theme.taglist_squares_unsel = theme.dir.."/taglist/squarew.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = theme_dir.."/menu/submenu.png"
+theme.menu_submenu_icon = theme.dir.."/menu/submenu_grey.png"
 theme.menu_border_width = 3
 theme.menu_border_color = theme.bg_normal
-theme.menu_height = 14
+theme.menu_height = 15
 theme.menu_width  = 100
--- Menu icons
-theme.menu_awesome = theme_dir.."/menu/awesome.png"
-theme.menu_apps = theme_dir.."/menu/apps.png"
-theme.menu_info = theme_dir.."/menu/info.png"
-theme.menu_terminal = theme_dir.."/menu/terminal.png"
-theme.menu_reboot = theme_dir.."/menu/reboot.png"
-theme.menu_shutdown = theme_dir.."/menu/shutdown.png"
-theme.menu_logout = theme_dir.."/menu/logout.png"
-theme.menu_accept = theme_dir.."/menu/accept.png"
-theme.menu_cancel = theme_dir.."/menu/cancel.png"
-theme.menu_lock = theme_dir.."/menu/lock.png"
+theme.menu_awesome = theme.dir.."/menu/awesome.png"
+theme.menu_apps = theme.dir.."/menu/apps.png"
+theme.menu_info = theme.dir.."/menu/info.png"
+theme.menu_terminal = theme.dir.."/menu/terminal.png"
+theme.menu_reboot = theme.dir.."/menu/reboot.png"
+theme.menu_shutdown = theme.dir.."/menu/shutdown.png"
+theme.menu_logout = theme.dir.."/menu/logout.png"
+theme.menu_accept = theme.dir.."/menu/accept.png"
+theme.menu_cancel = theme.dir.."/menu/cancel.png"
+theme.menu_lock = theme.dir.."/menu/lock.png"
 
 -- You can add as many variables as
 -- you wish and access them by using
@@ -141,130 +108,83 @@ theme.menu_lock = theme_dir.."/menu/lock.png"
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = theme_dir.."/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = theme_dir.."/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = theme.dir.."/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = theme.dir.."/titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = theme_dir.."/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = theme_dir.."/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = theme_dir.."/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = theme_dir.."/titlebar/ontop_focus_active.png"
+theme.titlebar_minimize_button_normal = theme.dir.."/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus  = theme.dir.."/titlebar/minimize_focus.png"
 
-theme.titlebar_sticky_button_normal_inactive = theme_dir.."/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = theme_dir.."/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = theme_dir.."/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = theme_dir.."/titlebar/sticky_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = theme.dir.."/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = theme.dir.."/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = theme.dir.."/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = theme.dir.."/titlebar/ontop_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = theme_dir.."/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = theme_dir.."/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = theme_dir.."/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = theme_dir.."/titlebar/floating_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = theme.dir.."/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = theme.dir.."/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = theme.dir.."/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = theme.dir.."/titlebar/sticky_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = theme_dir.."/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = theme_dir.."/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = theme_dir.."/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = theme_dir.."/titlebar/maximized_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = theme.dir.."/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = theme.dir.."/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = theme.dir.."/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = theme.dir.."/titlebar/floating_focus_active.png"
 
-theme.titlebar_minimize_button_normal_inactive = theme_dir.."/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus_inactive  = theme_dir.."/titlebar/minimize_focus.png"
+theme.titlebar_maximized_button_normal_inactive = theme.dir.."/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.dir.."/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = theme.dir.."/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = theme.dir.."/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = theme_dir.."/background.png"
+theme.wallpaper = theme.dir.."/background.png"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = theme_dir.."/layouts/fairh.png"
-theme.layout_fairv = theme_dir.."/layouts/fairv.png"
-theme.layout_floating  = theme_dir.."/layouts/floating.png"
-theme.layout_magnifier = theme_dir.."/layouts/magnifier.png"
-theme.layout_max = theme_dir.."/layouts/max.png"
-theme.layout_fullscreen = theme_dir.."/layouts/fullscreen.png"
-theme.layout_tilebottom = theme_dir.."/layouts/tilebottom.png"
-theme.layout_tileleft   = theme_dir.."/layouts/tileleft.png"
-theme.layout_tile = theme_dir.."/layouts/tile.png"
-theme.layout_tiletop = theme_dir.."/layouts/tiletop.png"
-theme.layout_spiral  = theme_dir.."/layouts/spiral.png"
-theme.layout_dwindle = theme_dir.."/layouts/dwindle.png"
+theme.layout_fairh = theme.dir.."/layouts/fairhw.png"
+theme.layout_fairv = theme.dir.."/layouts/fairvw.png"
+theme.layout_floating  = theme.dir.."/layouts/floatingw.png"
+theme.layout_magnifier = theme.dir.."/layouts/magnifierw.png"
+theme.layout_max = theme.dir.."/layouts/maxw.png"
+theme.layout_fullscreen = theme.dir.."/layouts/fullscreenw.png"
+theme.layout_tilebottom = theme.dir.."/layouts/tilebottomw.png"
+theme.layout_tileleft   = theme.dir.."/layouts/tileleftw.png"
+theme.layout_tile = theme.dir.."/layouts/tilew.png"
+theme.layout_tiletop = theme.dir.."/layouts/tiletopw.png"
+theme.layout_spiral  = theme.dir.."/layouts/spiralw.png"
+theme.layout_dwindle = theme.dir.."/layouts/dwindlew.png"
+theme.layout_cornernw = theme.dir.."/layouts/cornernww.png"
+theme.layout_cornerne = theme.dir.."/layouts/cornernew.png"
+theme.layout_cornersw = theme.dir.."/layouts/cornersww.png"
+theme.layout_cornerse = theme.dir.."/layouts/cornersew.png"
 
-theme.awesome_icon = theme_dir .. "/awesome.png"
+theme.awesome_icon = theme.dir.."/menu/awesome.png"
 
-
--- Define the icon theme for application icons. If not set then the icons 
+-- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
---theme.icon_theme = "~/.icons/AwOkenDark" 
-theme.blingbling = {
-    background_color = transparent,
-    graph_background_color = widget_background,
-    graph_color = bright_magenta,
-    graph_line_color = magenta,
-    rounded_size = 0.3,        
-    text_color = light_white,
-    --reboot = theme_dir .. "/reboot.png",
-    --shutdown = theme_dir .. "/shutdown.png",
-    --logout = theme_dir .. "/logout.png",
-    --accept = theme_dir .. "/ok.png",
-    --cancel = theme_dir .. "/cancel.png",
-    --lock = theme_dir .. "/lock.png",
-    font = "Droid Sans Mono",
-    font_size = 9 
+theme.icon_theme = nil
+
+-- Calendar widget
+local cell_style = {
+    h_margin = 0, 
+    v_margin = 0, 
+    rounded_size = 0.3, 
+    background_color = widget_background, 
+    text_background_color = transparent,
+    text_color = white, 
 }
 
+theme.calendar_style = calendar_style
+theme.calendar_days_of_week_text_color       = light_grey
+theme.calendar_weeks_number_text_color       = light_grey
+theme.calendar_corner_widget_text_color      = light_grey
 
-theme.blingbling.tagslist = {}
-theme.blingbling.tagslist.normal ={ background_color = widget_background,--rgb(26,26,26),
-                                    text_background_color = "#00000000", --no color
-                                    rounded_size = { 0, 0.4,0,0.4 },
-                                    text_color = theme.fg_normal,
-                                    font = "Droid Sans",
-                                    font_size = 7 
-                                  }
-theme.blingbling.tagslist.focus = { h_margin = 1,
-                                    v_margin = 1,
-                                    background_color = red, 
-                                    text_background_color = widget_background,
-                                    text_color = theme.fg_normal,
-                                    rounded_size = { 0, 0.4,0,0.4 },
-                                    font = "Droid Sans italic",
-                                    font_size = 8
-                                  }
+theme.calendar_current_day_background_color  = green
+theme.calendar_current_day_text_color        = dark_grey
+theme.calendar_current_day_rounded_size      = {0.5,0,0.5,0}
 
-theme.blingbling.tagslist.urgent = theme.blingbling.tagslist.focus
-theme.blingbling.tagslist.occupied = theme.blingbling.tagslist.normal
+theme.calendar_focus_widget_background_color = yellow
+theme.calendar_focus_widget_rounded_size     = {0,0.5,0,0.5}
+theme.calendar_info_cell_background_color    = transparent
+theme.calendar_info_cell_text_color          = bright_white
 
-local cal_common_style = {  h_margin = 0, 
-                            v_margin = 0, 
-                            rounded_size = 0.3, 
-                            background_color = widget_background, 
-                            text_background_color = "#00000000",
-                            text_color = white, 
-                            font ="Droid Sans"
-                          }
-theme.blingbling.calendar = {}
-local util = require('awful.util')
-theme.blingbling.calendar.prev_next_widget_style =
-util.table.clone(cal_common_style)    
-theme.blingbling.calendar.current_date_widget_style =
-util.table.clone(cal_common_style)
-theme.blingbling.calendar.days_of_week_widget_style =
-util.table.clone(cal_common_style)
-theme.blingbling.calendar.days_of_month_widget_style =
-util.table.clone(cal_common_style)
-theme.blingbling.calendar.weeks_number_widget_style =
-util.table.clone(cal_common_style)
-theme.blingbling.calendar.corner_widget_style =
-util.table.clone(cal_common_style)
-theme.blingbling.calendar.current_day_widget_style =
-util.table.clone(cal_common_style)
-theme.blingbling.calendar.focus_widget_style =
-util.table.clone(cal_common_style)
-
-theme.blingbling.calendar.info_cell_style = util.table.clone(cal_common_style)
-theme.blingbling.calendar.info_cell_style.background_color = transparent
-theme.blingbling.calendar.info_cell_style.text_color = bright_white
-theme.blingbling.calendar.current_day_widget_style.background_color = green
-theme.blingbling.calendar.current_day_widget_style.text_color = dark_grey
-theme.blingbling.calendar.current_day_widget_style.rounded_size = {0.5,0,0.5,0}
-theme.blingbling.calendar.focus_widget_style.background_color = yellow
-theme.blingbling.calendar.focus_widget_style.rounded_size = {0,0.5,0,0.5}
-theme.blingbling.calendar.days_of_week_widget_style.text_color = light_grey
-theme.blingbling.calendar.corner_widget_style.text_color = light_grey
-theme.blingbling.calendar.weeks_number_widget_style.text_color = light_grey 
 
 return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
